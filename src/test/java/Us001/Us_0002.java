3a4fd38ab5b34f1eca1e5b3c36d5996537d8c232
SZGN <szgnmhmmd@gmail.com>
2022-05-24 00:10:09 +0300

    Us_0002 eklendi

--- src/test/java/Us001/Us_0002.java
+++ src/test/java/Us001/Us_0002.java
@@ -1,4 +1,85 @@
 package Us001;
 
-public class Us_0002 {
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import utilities.TestBase;
+
+public class Us_0002 extends TestBase {
+
+    @Test
+    public void test01() {
+        driver.get("https://qa-environment.concorthotel.com");
+        WebElement login = driver.findElement(By.xpath("//a[normalize-space()='Log in']"));
+        Assert.assertTrue(login.isDisplayed());
+        login.click();
+    }
+
+    @Test
+    public void test02() {
+        driver.get("https://qa-environment.concorthotel.com");
+        WebElement login = driver.findElement(By.xpath("//a[normalize-space()='Log in']"));
+        Assert.assertTrue(login.isDisplayed());
+        login.click();
+        WebElement userName=driver.findElement(By.xpath("//input[@id='UserName']"));
+        Assert.assertTrue(userName.isDisplayed());
+        userName.sendKeys("Admin");
+    }
+
+    @Test
+    public void test03() {
+        driver.get("https://qa-environment.concorthotel.com");
+        WebElement login = driver.findElement(By.xpath("//a[normalize-space()='Log in']"));
+        Assert.assertTrue(login.isDisplayed());
+        login.click();
+        WebElement userName=driver.findElement(By.xpath("//input[@id='UserName']"));
+        Assert.assertTrue(userName.isDisplayed());
+        userName.sendKeys("Admin");
+        WebElement password=driver.findElement(By.xpath("//input[@id='Password']"));
+        Assert.assertTrue(password.isDisplayed());
+        password.sendKeys("Admin");
+    }
+
+    @Test
+    public void test04() {
+        driver.get("https://qa-environment.concorthotel.com");
+        WebElement login = driver.findElement(By.xpath("//a[normalize-space()='Log in']"));
+        Assert.assertTrue(login.isDisplayed());
+        login.click();
+        WebElement userName=driver.findElement(By.xpath("//input[@id='UserName']"));
+        Assert.assertTrue(userName.isDisplayed());
+        userName.sendKeys("Manager");
+        WebElement password=driver.findElement(By.xpath("//input[@id='Password']"));
+        Assert.assertTrue(password.isDisplayed());
+        password.sendKeys("manager1!");
+        driver.findElement(By.xpath("//input[@id='btnSubmit']")).click();
+        WebElement userName1 =driver.findElement(By.xpath("//span[@class='username username-hide-on-mobile']"));
+        actions.moveToElement(userName1).perform();
+        driver.findElement(By.xpath("//a[normalize-space()='Log out']")).click();
+
+
+
+
+
+    }
+
+    @Test
+    public void test05() {
+        driver.get("https://qa-environment.concorthotel.com");
+        WebElement login = driver.findElement(By.xpath("//a[normalize-space()='Log in']"));
+        Assert.assertTrue(login.isDisplayed());
+        login.click();
+        WebElement userName=driver.findElement(By.xpath("//input[@id='UserName']"));
+        Assert.assertTrue(userName.isDisplayed());
+        userName.sendKeys("Manager");
+        WebElement password=driver.findElement(By.xpath("//input[@id='Password']"));
+        Assert.assertTrue(password.isDisplayed());
+        password.sendKeys("manager1!");
+        driver.findElement(By.xpath("//input[@id='btnSubmit']")).click();
+        WebElement users =driver.findElement(By.xpath("//span[@class='caption-subject font-green-sharp bold uppercase']"));
+        Assert.assertTrue(users.isDisplayed());
+
+
+    }
 }
--- src/test/java/utilities/TestBase.java
+++ src/test/java/utilities/TestBase.java
@@ -2,11 +2,14 @@ package utilities;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.After;
+import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
 import java.io.File;
@@ -16,6 +19,7 @@ public abstract class TestBase {
     // abstract yapmamizin sebebi, bu class'dan obje uretilmesinin onune gecmektir.
     protected static WebDriver driver;
     protected  static ChromeOptions options;
+    protected static Actions actions;
     @BeforeClass
     public static void setUp(){
         WebDriverManager.chromedriver().setup();
@@ -23,13 +27,18 @@ public abstract class TestBase {
         options.addExtensions(new File("C:\\Program Files\\Google\\Chrome\\Application\\101.0.4951.67\\XPath-Plugin.crx"));
         DesiredCapabilities capabilities = new DesiredCapabilities();
         capabilities.setCapability(ChromeOptions.CAPABILITY, options);
-        ChromeDriver driver = new ChromeDriver(capabilities);
+        driver = new ChromeDriver(capabilities);
+        actions =new Actions(driver);
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        driver.get("https://qa-environment.concorthotel.com");
+        driver.findElement(By.xpath("//*[@id='details-button']")).click();
+        driver.findElement(By.xpath("//*[@id='proceed-link']")).click();
+
     }
 
-    @After
-    public void tearDown(){
-        driver.quit();
+    @AfterClass
+    public static void tearDown(){
+      // driver.quit();
     }
 }
